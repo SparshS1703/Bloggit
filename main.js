b8fd85b14dbd828a6c1a15b9a56e816759812739
Sparsh <1711sparsh@gmail.com>
2024-09-24 19:51:10 +0530

    http

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ app.use(session({
   secret: '123456sparsh', // Replace with a random string
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: true } // Set to true if using HTTPS
+  cookie: {  secure: process.env.NODE_ENV === 'production' } // Set to true if using HTTPS
 }));
 
 
